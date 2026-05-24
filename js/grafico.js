994079b1a35da6ec953c1603ab69a41a1fbba914
ProgramadoraDaina <Daina.Paucar@alumnos.frgp.utn.edu.ar>
2026-05-24 16:43:37 -0300

    feat: improve UI layout, enhance sidebar design, and optimize chart responsiveness

--- js/grafico.js
+++ js/grafico.js
@@ -35,7 +35,7 @@ export function dibujarGrafico(labels, data) {
 
         options: {
             responsive: true,
-
+            maintainAspectRatio: false,
             plugins: {
                 legend: {
                     labels: {
